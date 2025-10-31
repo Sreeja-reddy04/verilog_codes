4f0aaaf55f48b23f8b0cf122be0684cf59427e35
sreeja kunta <142441382+Sreeja-reddy04@users.noreply.github.com>
2025-10-31 09:38:57 +0530

    Update jkff_tb.v

--- jkff_tb.v
+++ jkff_tb.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   11:14:26 10/15/2025
-// Design Name:   jff
-// Module Name:   /home/vboxuser/Documents/3436-verilog_labs_f/Verilog_labs/lab4/tb/jkff_tb.v
-// Project Name:  jkff
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: jff
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module jkff_tb;
 
 	// Inputs
