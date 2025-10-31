fd48ab1deef39df6e8d82501db626f8b51017496
sreeja kunta <142441382+Sreeja-reddy04@users.noreply.github.com>
2025-10-31 09:39:54 +0530

    Update priority_encoder.v

--- priority_encoder.v
+++ priority_encoder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:08:47 10/10/2025 
-// Design Name: 
-// Module Name:    priority_encoder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module priority_encoder(y0,y1,y2,IDLE,d0,d1,d2,d3,d4,d5,d6,d7);
 input d0,d1,d2,d3,d4,d5,d6,d7;
 output y0,y1,y2,IDLE;
